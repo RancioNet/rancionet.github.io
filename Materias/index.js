fbf95c7f90c1f68a60cabc3c55dd314fba135d75
RancioNet <salvadorrodrig568+@gmail.com>
2024-04-20 11:53:20 -0300

    On branch main  Your branch is up to date with 'origin/main'.
     Changes to be committed:
    	modified:   Materias/efemerides.html
    	modified:   Materias/index.css
    	new file:   Materias/index.js
    	modified:   Materias/ingles.html
    	modified:   Materias/lengua.html
    	modified:   Materias/matematicas.html
    	modified:   Materias/naturales.html
    	modified:   Materias/sociales.html

---
+++ Materias/index.js
@@ -0,0 +1,9 @@
+function Buscar() {
+    const searchTerm = prompt('Ingrese el término de búsqueda:'); // Obtener el término de búsqueda
+    if (!searchTerm) return; // Si no se ingresó un término de búsqueda, salir de la función
+
+    const contenido = document.getElementById('fondo').innerHTML; // Obtener el contenido del div "fondo"
+    const contenidoResaltado = contenido.replace(new RegExp(searchTerm, 'gi'), match => `<span class="highlight">${match}</span>`); // Resaltar las coincidencias con el término de búsqueda
+
+    document.getElementById('fondo').innerHTML = contenidoResaltado; // Actualizar el contenido con las coincidencias resaltadas
+}
