77b8e7a9ec7c226a29b2c99a454258ef7a5002c2
xielbers <salvadorrodrig568@gmail.com>
2024-05-23 08:47:52 -0300

    22 de mayo

--- traductor.js
+++ traductor.js
@@ -1,43 +1,82 @@
-let toNewAlphabet = true;
-
-function translateText() {
-    toNewAlphabet = true;
-    translate();
-}
-
-function translateTextReverse() {
-    toNewAlphabet = false;
-    translate();
-}
+let toMystiqueAlphabet = true;
 
 function translate() {
     const inputText = document.getElementById('inputText').value;
     const outputText = document.getElementById('outputText');
     
     const normalAlphabet = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
-    const newAlphabet = 'CDEFGHIJKLMNOPQRSTUVWXYZAB';
+    const mystiqueAlphabet = 'CDEFGHIJKLMNOPQRSTUVWXYZAB';
+    
+    const specialChars = ['?', '!', '¿', '¡']; // Caracteres especiales para invertir
     
     let translatedText = '';
-    
+    let specialCharPositions = [];
+    let normalCharPositions = [];
+
+    // Recoger posiciones de caracteres especiales y construir el texto a traducir sin estos caracteres
     for (let i = 0; i < inputText.length; i++) {
-        let char = inputText[i].toUpperCase();
+        if (specialChars.includes(inputText[i])) {
+            specialCharPositions.push({ char: inputText[i], position: i });
+        } else if (/[0-9]/.test(inputText[i])) {
+            normalCharPositions.push({ char: inputText[i], position: i });
+        } else {
+            translatedText += inputText[i];
+        }
+    }
+
+    // Traducir caracteres normales
+    let translatedNormalText = '';
+    for (let i = 0; i < translatedText.length; i++) {
+        let char = translatedText[i].toUpperCase();
         let index;
-        if (toNewAlphabet) {
+        if (toMystiqueAlphabet) {
             index = normalAlphabet.indexOf(char);
             if (index !== -1) {
-                translatedText += newAlphabet[index];
+                translatedNormalText += mystiqueAlphabet[index];
             } else {
-                translatedText += char;
+                translatedNormalText += char;
             }
         } else {
-            index = newAlphabet.indexOf(char);
+            index = mystiqueAlphabet.indexOf(char);
             if (index !== -1) {
-                translatedText += normalAlphabet[index];
+                translatedNormalText += normalAlphabet[index];
             } else {
-                translatedText += char;
+                translatedNormalText += char;
             }
         }
     }
-    
-    outputText.textContent = translatedText;
+
+    // Reinsertar caracteres especiales en sus posiciones originales invertidos
+    for (let i = 0; i < specialCharPositions.length; i++) {
+        let pos = specialCharPositions[i].position;
+        let char = specialCharPositions[specialCharPositions.length - 1 - i].char;
+        translatedNormalText = translatedNormalText.slice(0, pos) + char + translatedNormalText.slice(pos);
+    }
+
+    // Reinsertar números cifrados en sus posiciones originales
+    for (let i = 0; i < normalCharPositions.length; i++) {
+        let pos = normalCharPositions[i].position;
+        let char = normalCharPositions[i].char;
+        let numChar = parseInt(char, 10);
+        
+        if (toMystiqueAlphabet) {
+            numChar = (numChar + 2) % 10;
+        } else {
+            numChar = (numChar - 2 + 10) % 10;
+        }
+        
+        translatedNormalText = translatedNormalText.slice(0, pos) + numChar.toString() + translatedNormalText.slice(pos);
+    }
+
+    outputText.textContent = translatedNormalText;
+}
+
+function translateToMystique() {
+    toMystiqueAlphabet = true;
+    translate();
+}
+
+function translateToNormal() {
+    toMystiqueAlphabet = false;
+    translate();
 }
