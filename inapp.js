c503877733242aab89524d9df52beb898ff4865f
xielbers <salvadorrodrig568@gmail.com>
2024-05-29 13:39:54 -0300

    29 de mayo
    Matemáticas

--- inapp.js
+++ inapp.js
@@ -1,16 +1,19 @@
 function InAPP(versionCliente) {
-    var Elementos = [document.getElementById("down")];
+    var Elemento = document.getElementById("down");
     var Download = document.getElementById('appActualizacion');
 
     var versionNecesaria = '28/5/24';
     
-    // Verificar si el elemento existe
-    if (Elementos[0] && versionCliente == versionNecesaria) {
+    // Verificar si el elemento existe y si la versión del cliente es correcta
+    if (Elemento && versionCliente === versionNecesaria) {
         // Eliminar el elemento
-        Elementos[0].parentNode.removeChild(Elementos[0]);
+        Elemento.parentNode.removeChild(Elemento);
         console.log("Versión correcta");
     } else {
         console.log("Versión incorrecta");
+        // Evitar el scroll
+        document.body.style.overflow = 'hidden';
+        // Mostrar el div de actualización
         Download.style.display = 'flex';
     }
 }
