4b7e1e3fc467060140b0e1215baddbde19ad69d7
xielbers <salvadorrodrig568@gmail.com>
2024-06-22 17:08:29 -0300

    22

--- inapp.js
+++ inapp.js
@@ -2,7 +2,7 @@ function InAPP(versionCliente) {
     var Elemento = document.getElementById("down");
     var Download = document.getElementById('appActualizacion');
 
-    var versionNecesaria = '16/6/24';
+    var versionNecesaria = '22/6/24';
     
     // Verificar si el elemento existe y si la versión del cliente es correcta
     if (Elemento && versionCliente === versionNecesaria) {
@@ -10,7 +10,7 @@ function InAPP(versionCliente) {
         Elemento.parentNode.removeChild(Elemento);
         console.log("Versión correcta");
     } else {
-        console.log("Versión incorrecta");
+        console.error("Versión incorrecta");
         // Evitar el scroll
         document.body.style.overflow = 'hidden';
         // Mostrar el div de actualización
